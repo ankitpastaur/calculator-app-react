8b284754380a4fa6403e5c387e471d36ac913258
ankitpastor <pastorankit18@gmail.com>
2025-03-04 23:45:38 -0500

    Adding basic functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,39 @@
+import { useState } from 'react'
 import './App.css'
 
 function App() {
   const arr = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '+', '-', '*', '/', '=', 'C', '.']
+  const [value, setValue]  = useState('');
+
+  const handleChange = (e) => {
+    setValue(e.target.value)
+  }
+  console.log(value);
+  
+  const handleClick = (e) => {
+    const id = e.target.id
+    console.log((id));
+    if(id === 'C'){
+      setValue('')
+    }
+    else if(id === '='){
+      //Produce result
+    }
+    else{
+      setValue((val)=> val+id)
+    }
+  }
+
   return (
     <div className='App'>
       <h1>Calculator</h1>
       <form>
-        <input type='text' />
+        <input type='text' onChange={handleChange} value={value}/>
       </form>
-      <div className='container'>
+      <div className='container' onClick={handleClick}>
         {
           arr.map((item, idx)=>{
-            return <button id ={item}key={idx} className='cell'>{item} </button>
+            return <button id ={item} key={idx} className='cell'>{item} </button>
           })
         }
       </div>
